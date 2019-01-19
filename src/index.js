a5561e2493cec42a1cd4eade4316658c0a7ad6a4
James Lee <jamesleehawaii@gmail.com>
2019-01-19 12:31:34 -0500

    react starterpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>All the React are belong to us!</div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
